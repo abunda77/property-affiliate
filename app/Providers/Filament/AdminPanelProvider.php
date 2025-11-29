697c939e55bb8ad7805f3477835f431ac901de96
unknown <erieputranto@gmail.com>
2025-11-29 17:18:33 +0700

    cooment shield

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -58,24 +58,24 @@ class AdminPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ])
-            ->plugins([
-                FilamentShieldPlugin::make()
-                    ->gridColumns([
-                        'default' => 1,
-                        'sm' => 2,
-                        'lg' => 3,
-                    ])
-                    ->sectionColumnSpan(1)
-                    ->checkboxListColumns([
-                        'default' => 1,
-                        'sm' => 2,
-                        'lg' => 4,
-                    ])
-                    ->resourceCheckboxListColumns([
-                        'default' => 1,
-                        'sm' => 2,
-                    ]),
-            ])
+            // ->plugins([
+            //     FilamentShieldPlugin::make()
+            //         ->gridColumns([
+            //             'default' => 1,
+            //             'sm' => 2,
+            //             'lg' => 3,
+            //         ])
+            //         ->sectionColumnSpan(1)
+            //         ->checkboxListColumns([
+            //             'default' => 1,
+            //             'sm' => 2,
+            //             'lg' => 4,
+            //         ])
+            //         ->resourceCheckboxListColumns([
+            //             'default' => 1,
+            //             'sm' => 2,
+            //         ]),
+            // ])
             ->authMiddleware([
                 Authenticate::class,
                 \App\Http\Middleware\CheckUserStatus::class,
